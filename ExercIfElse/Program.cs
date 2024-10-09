c559a089d3abafe73bdd85977a3ef652c405bb2e
hilton.lssantos <hilton.lssantos@RIP0576973W10-1>
2024-10-09 13:54:55 -0300

    Adicionar arquivos de projeto.

---
+++ ExercIfElse/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercIfElse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+decimal nota1, nota2, nota3, media;
+
+            Console.WriteLine("Informe a nota1: ");
+            nota1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe a nota2: ");
+            nota2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("informe a nota3: ");
+            nota3 = Convert.ToDecimal(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado!");
+                Console.WriteLine("Media: " + media);
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!");
+                Console.WriteLine("Media: " + media);
+            }
+            Console.WriteLine("Calcule a media: " + media);
+            media = Convert.ToDecimal(Console.ReadLine());
+
+
+            Console.ReadKey();
+        }
+    }
+}
